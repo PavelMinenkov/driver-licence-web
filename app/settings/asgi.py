b34bac23b2f0d4608e8d79c947ee29cca42e35c6
Pavel <minikspb@gmail.com>
2022-01-19 01:01:39 +0300

    add websocket support

--- app/settings/asgi.py
+++ app/settings/asgi.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+from django.urls import re_path
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+
+from websockets.consumers import QRNotifications
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.production')
 
@@ -23,5 +26,7 @@ application = ProtocolTypeRouter({
     "http": django_asgi_app,
 
     # WebSocket handler
-    "websocket": URLRouter([])
+    "websocket": URLRouter([
+        re_path("^qr-notification$", QRNotifications),
+    ])
 })
