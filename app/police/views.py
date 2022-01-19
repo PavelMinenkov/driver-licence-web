02ad49fe901ef1170f29ad6d4eccedf86899de2e
Mikhail Lytaev <mikelytaev@gmail.com>
2022-01-19 19:23:13 +0300

    experiments with django forms

--- app/police/views.py
+++ app/police/views.py
@@ -1,10 +1,22 @@
 from django.http import HttpResponse
 from django.template import loader
+from django import forms
+
+
+class IssueDriverLicenseForm(forms.Form):
+    last_name = forms.CharField()
+    first_name = forms.CharField()
+    birthday = forms.DateField()
 
 
 async def index(request):
-    template = loader.get_template('index.police.html')
-    context = {
-        'title': 'Police'
-    }
-    return HttpResponse(template.render(context, request))
+    if request.method == 'POST':
+        form = IssueDriverLicenseForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data['first_name'])
+    else:
+        template = loader.get_template('index.police.html')
+        context = {
+            'title': 'Police'
+        }
+        return HttpResponse(template.render(context, request))
